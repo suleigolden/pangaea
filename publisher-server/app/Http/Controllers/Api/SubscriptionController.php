3dac368f06f1b8b1fd6de46e583d75d1d885ce24
Suleiman Mamman <suleigolden>
2021-04-02 07:24:00 +0100

    subcription end point completed

--- publisher-server/app/Http/Controllers/Api/SubscriptionController.php
+++ publisher-server/app/Http/Controllers/Api/SubscriptionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Subscriber;
 
 class SubscriptionController extends Controller
 {
@@ -17,12 +19,26 @@ class SubscriptionController extends Controller
     public function subscribe(Request $request)
     {
 
+        $validator = Validator::make($request->all(), [
+            'url'   => 'required|url|max:250'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'  => 'error',
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        Subscriber::create($request->all());
         $data = [
             'url'   => $request->url,
             'topic' => $request->topic
         ];
 
         return response()->json($data, 201);
+        
+    
     }
 
 }
